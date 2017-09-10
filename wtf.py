3b29b93ae458c9195b6569fe48f28e779a3a9ef8
JaniJaala <31827427+JaniJaala@users.noreply.github.com>
2017-09-10 21:12:03 +0300

    wtf

---
+++ wtf.py
@@ -0,0 +1,120 @@
+# Database connection
+def dbc:
+
+
+# Input handler
+def inh:
+
+
+# Examine command
+def examine:
+
+
+# Inventory command
+def inventory:
+
+
+# Give command
+def give:
+
+
+# Use command
+def use:
+
+
+# Call Mario command
+def callmario:
+
+
+# Ask Mario command
+def askmario:
+
+
+# Help command
+def help:
+
+
+# Walk command
+def walk:
+
+
+# Go to command
+def goto:
+
+
+# Value command
+def value:
+
+
+# Sell command
+def sell:
+
+
+# Buy command
+def buy:
+
+
+# Kick command
+def kick:
+
+
+# Function for rng's
+def rng:
+
+
+# Get player location
+def gpl:
+
+
+# Set player location
+def spl:
+
+
+# Player has item
+def hasitem:
+
+
+# get Dialogue completed
+def getdialcomp:
+
+
+# set dialogue completed
+def setdialcomp:
+
+
+# get Dialogue locked
+def getdiallock:
+
+
+# set dialogue locked
+def setdiallock:
+
+
+# Summon chuck norris
+def chuck:
+
+
+# Get dialogue
+def getdial:
+
+
+# npc has item
+def npcitem:
+
+
+# set Mario psychotic
+def getpsych:
+
+
+# get Mario psychotic
+def setpsych:
+
+
+# get objects
+def getobj:
+
+
+# object has item
+def objitem:
+
+
